a9c3c029dcb0e517a5c4b9be91987ae8c379e593
Vinirsp <Viniciusrsp7@gmail.com>
2025-05-30 12:53:41 -0300

    Add files via upload

---
+++ Urna/Urna/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using urna;
+
+namespace URNA_ELETRONICA
+{
+    public partial class Form1 : Form
+    {
+        string num;
+
+        int votosBranco = 0;
+        int votosNulos = 0;
+        int votos10 = 0;
+        int votos7 = 0;
+        int votos11 = 0;
+
+        int candidato10, candidato20, candidato30, branco, nulo;
+        int a = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumero_Click(object sender, EventArgs e)
+        {
+            Button bt = (Button)sender;
+            txtNumero.Text += bt.Text;
+        }
+
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+            num = txtNumero.Text;
+
+            if (num == "10")
+            {
+                picFoto.Image = Properties.Resources.Messi;
+                lblCandidato.Text = "Messi";
+
+            }
+
+            else if (num == "7")
+            {
+                picFoto.Image = Properties.Resources.Ronaldo;
+                lblCandidato.Text = "Ronaldo";
+            }
+
+            else if (num == "11")
+            {
+                picFoto.Image = Properties.Resources.Neymar;
+                lblCandidato.Text = "Neymar";
+            }
+            else if (num != "1" && num != "2" && num != "3" && num != "4" && num != "5" && num != "6" &&
+                  num != "7" && num != "8" && num != "9" && num != "0" & num != "--")
+            {
+            }
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            var resultado = new Form2(votos10, votos7, votos11, votosBranco, votosNulos);
+            resultado.ShowDialog();
+        }
+
+        private void btnCorrige_Click(object sender, EventArgs e)
+        {
+            txtNumero.Clear();
+            lblCandidato.Text = "";
+            picFoto.Image = null;
+        }
+
+        private void btnBranco_Click(object sender, EventArgs e)
+        {
+            txtNumero.Text = "--";
+            lblCandidato.Text = "Voto em Branco";
+            picFoto.Image = null;
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            if (num == "10")
+            {
+                votos10++;
+                MessageBox.Show("Voto confirmado para Messi");
+            }
+            else if (num == "7")
+            {
+                votos7++;
+                MessageBox.Show("Voto confirmado para Ronaldo");
+            }
+            else if (num == "11")
+            {
+                votos11++;
+                MessageBox.Show("Voto confirmado para Neymar");
+            }
+            else if (num == "--")
+            {
+                votosBranco++;
+                MessageBox.Show("Voto em branco confirmado!");
+            }
+            else
+            {
+                votosNulos++;
+                MessageBox.Show("Voto nulo!");
+            }
+
+            txtNumero.Clear();
+            lblCandidato.Text = "";
+            picFoto.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Urna/Urna/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace urna
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        public Form2(int votos10, int votos7, int votos11, int votosBranco, int votosNulos)
+        {
+            InitializeComponent();
+
+            label1.Text = $"Messi: {votos10}";
+            label2.Text = $"Ronaldo: {votos7}";
+            label3.Text = $"Neymar: {votos11}";
+            label4.Text = $"branco: {votosBranco}";
+            label5.Text = $"Nulo: {votosNulos}";
+        }
+    }
+}
